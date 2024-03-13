4303b14787a333dcac084f6f8d64f8b39cdad19e
md Sadique <sadiquekhan08200@example.com>
2024-03-13 22:47:18 +0530

    first commit

---
+++ src/components/projectlist.js
@@ -0,0 +1,62 @@
+ export const projectList = [
+
+    {
+        title: 'React_E-Commerce Web Application',
+        created: 'Oct 2023 - Dec 2023',
+        description: 'The E-Commerce Application project is a dynamic web platform developed using the React.js framework, leveraging the Fetch API for seamless data retrieval and management. With a focus on user experience and functionality, this project aims to provide a robust e-commerce solution equipped with essential features for online shopping.',
+        demo_link: 'https://react-e-commerce-ao78eqs67-tonystarkk08s-projects.vercel.app',
+        github_link: 'https://github.com/tonystarkk08/e-commerce'
+    },
+    {
+        title: 'Discord-Clone   ',
+        created: 'dec 2023 – feb2024',
+        description: `The Discord Clone project is a web application created with React.js, 
+        React Router DOM, and NavLink. As a passionate developer, I undertook this project to 
+        demonstrate my proficiency in frontend development and my ability to create dynamic and interactive web applications.`,
+        demo_link: 'https://discord-clone-jlso2qacc-tonystarkk08s-projects.vercel.app ',
+        github_link: 'https://github.com/tonystarkk08/Discord-Clone'
+    },
+  
+    {
+        title: 'Quick-Shop',
+        created: 'Feb 2024 - currently working',
+        description: `The E-Commerce Application is a dynamic web platform developed using the React.js framework, featuring seamless data fetching with Axios and intuitive navigation using React Router DOM and NavLink. This project demonstrates my proficiency in frontend development and my ability to create robust and user-friendly e-commerce solutions.`,
+        demo_link: 'https://quick-shop-5q1w107z1-tonystarkk08s-projects.vercel.app ',
+        github_link: 'https://github.com/tonystarkk08/Quick-Shop'
+    },
+    // {
+    //     title: 'mathApi',
+    //     created: 'Nov 2019',
+    //     description: 'Handwritten mathematical expression evaluator. Developed an API, using flask, that receives a base64 encoded image, reads it and extracts the numbers and symbols out of it and then performs calculations and returns the output. Uses a deep learning model and MNIST dataset for training.',
+    //     demo_link: '',
+    //     github_link: 'https://github.com/savezsiddiqui/mathApi'
+    // },
+    // {
+    //     title: 'College Time Table',
+    //     created: 'Aug 2019',
+    //     description: `My college always had a very stupid excel sheet passed on to students from which you have to carefully highlight each of your electives and core subjects to get a time table that's readable. This project uses a unique regex to pull out your very own time table and displays it in a beautiful modern day format. The app is also a PWA so you can install it on your phone.`,
+    //     demo_link: 'https://savezsiddiqui.github.io/time-table2/',
+    //     github_link: 'https://github.com/savezsiddiqui/time-table2/'
+    // },
+    // {
+    //     title: 'International Tic Tac Toe',
+    //     created: 'NA',
+    //     description: 'It allows you to play tic tac toe with friends',
+    //     demo_link: 'https://savezsiddiqui.github.io/international-tic-tac-toe/',
+    //     github_link: 'https://github.com/savezsiddiqui/international-tic-tac-toe/'
+    // },
+    // {
+    //     title: 'Digipodium',
+    //     created: 'NA',
+    //     description: 'A fully responsive website. Passes all lighthouse tests',
+    //     demo_link: 'https://savezsiddiqui.github.io/digipodium/',
+    //     github_link: 'https://github.com/savezsiddiqui/digipodium/'
+    // },
+    // {
+    //     title: 'Echo music player',
+    //     created: 'NA',
+    //     description: 'A fully functional android music player with a unique equalizer',
+    //     demo_link: 'https://drive.google.com/file/d/1oM8iDONPjEIN2-qwmrhRQBMTuKp0X4cM/view?usp=sharing',
+    //     github_link: 'https://github.com/savezsiddiqui/echo-music-player'
+    // }
+]
